7c4043de996cf76b64ef1ba5aa145b4da3c99c40
SOYEONG OH <axionoso@gmail.com>
2019-03-19 04:03:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
+def runner(total)
   welcome
   initial_round
   until new_total == 22
